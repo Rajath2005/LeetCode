fc60b1df8eb7f92bb11ce917e9eb23c0765b66f8
Rajath Kiran A <4VP23CS070@vcetputtur.ac.in>
2026-09-06 23:37:22 +0530

    Time: 8 ms (7.15%) | Memory: 47.2 MB (29.07%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,25 +1,25 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-Stack<Integer> stack = new Stack<>();
+        Stack<Integer> stack = new Stack<>();
 
-for (int a : asteroids) {
-while (!stack.isEmpty() && a < 0 && stack.peek() > 0) {
-int diff = a + stack.peek();
-if (diff < 0) {
-stack.pop(); // Asteroid a survives, top of stack explodes
-continue;
-} else if (diff > 0) {
-a = 0; // Asteroid a explodes
-} else {
-stack.pop(); // Both explode
-a = 0;
-}
-}
-if (a != 0) {
-stack.push(a);
-}
-}
+        for (int a : asteroids) {
+            while (!stack.isEmpty() && a < 0 && stack.peek() > 0) {
+                int diff = a + stack.peek();
+                if (diff < 0) {
+                    stack.pop();
+                    continue;
+                } else if (diff > 0) {
+                    a = 0;
+                } else {
+                    stack.pop();
+                    a = 0;
+                }
+            }
+            if (a != 0) {
+                stack.push(a);
+            }
+        }
 
-return stack.stream().mapToInt(i -> i).toArray();
-}
+        return stack.stream().mapToInt(i -> i).toArray();
+    }
 }
